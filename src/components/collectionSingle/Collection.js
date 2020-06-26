91500a498058c24287d6dac90a73c16a4d862ae8
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-06-26 19:16:23 +0200

    Cleaned

--- src/components/collectionSingle/Collection.js
+++ src/components/collectionSingle/Collection.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import classes from './Collection.module.scss';
+import ReactImageAppear from 'react-image-appear';
 
 const Collection = (props) => {
     return (
         <div className={classes.Collection}>
-            <img src={`./images/${props.pathName}`} className={classes.img}></img>
+            <ReactImageAppear 
+                loader={props.loader}
+                src={`./images/${props.path}`}
+                animation="zoomIn"
+                animationDuration="1s"
+                className={classes.img}
+            /> 
             <h2>{props.name}</h2>
         </div>
     )
