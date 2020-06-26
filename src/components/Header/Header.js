ba8ec72ca04cac03101a4472bd3f92e1966c7cbd
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-06-26 19:16:45 +0200

    Header changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import classes from './Header.module.scss';
 const Header = (props) => {
     return (
         <div className={classes.Header}>
-            <h3> your collection</h3>
+            <h3> your collections</h3>
         </div>
     )
 };
