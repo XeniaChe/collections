3da954b3735bcce75b92f18e617ad6b9f2525b47
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-06-30 12:56:37 +0200

    Tags rendered. Recursion

---
+++ src/containers/collectionSingle/Collection.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import classes from './Collection.module.scss';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+// import images from '../../images';
+
+
+const Collection = (props) => {
+
+    const [state, setState] = useState({
+        tagsArray: [],
+        tagsReady: false
+    });
+
+    //dynamic style
+    let style = [classes.Collection];
+    //check if active 
+    if (props.clickedID === props.id) {
+        style.push(classes.Collection_active)
+    };
+
+    //Getting tags array method 
+    let tagsArray = []; 
+    const getSubtags = (obj, keyCheck) => {
+        for ( let key in obj) {
+            if( key === 'name'){
+                tagsArray.push(obj[key]);
+            }
+            if (keyCheck in obj &&  typeof obj[keyCheck] === 'object') {
+                getSubtags(obj[keyCheck], keyCheck);    
+                return;
+            } 
+        };
+
+        setState({
+            tagsArray: tagsArray,
+            tagsReady: true
+        })
+    };
+   
+    //Calling getting tags array method 
+    useEffect(() => {
+        let tagsImport = props.tags;
+        getSubtags(tagsImport, 'subTag');
+    },[]);
+    
+    let tags = null;
+    if (state.tagsReady) {
+        tags = state.tagsArray.map((el,index) => {
+            return (<p key={index}>{el}</p>);
+        }); 
+    }
+    
+    return (
+        <div className={style.join(' ')} onClick={props.click}>
+            <LazyLoadImage
+            className={classes.img}
+            src={`./images/${props.path}`}
+            // src={images`/${props.path}`}
+            effect="blur"/>
+            <h2>{props.name}</h2>
+            <div className={classes.Tags_Box}>
+                {tags}
+            </div>
+        </div>
+    )
+};
+
+export default Collection;
+
