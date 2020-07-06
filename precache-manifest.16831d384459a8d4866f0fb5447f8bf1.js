fa65ac3351cb84c824b90972701db7547efb72d4
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-07-06 13:07:33 +0200

    Updates

---
+++ precache-manifest.16831d384459a8d4866f0fb5447f8bf1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f39da3f2ee9e6d9f2388729902202a",
+    "url": "/collections/index.html"
+  },
+  {
+    "revision": "aab317d8ed564bbcb855",
+    "url": "/collections/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "8f8c7d90decf6fc4256c",
+    "url": "/collections/static/css/main.9bbc074a.chunk.css"
+  },
+  {
+    "revision": "aab317d8ed564bbcb855",
+    "url": "/collections/static/js/2.bf01c618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/collections/static/js/2.bf01c618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8c7d90decf6fc4256c",
+    "url": "/collections/static/js/main.6a441832.chunk.js"
+  },
+  {
+    "revision": "4fcc5aa3720ca3f73d91",
+    "url": "/collections/static/js/runtime-main.043e4bf0.js"
+  },
+  {
+    "revision": "5df5338d26d06ca8dead7d733b651258",
+    "url": "/collections/static/media/Bart.5df5338d.jpg"
+  },
+  {
+    "revision": "00c703bdf98345de31211d947e6a23e4",
+    "url": "/collections/static/media/Buzz.00c703bd.jpg"
+  },
+  {
+    "revision": "7f4d79026986f79060ab411a2d3b537e",
+    "url": "/collections/static/media/Flash.7f4d7902.jpg"
+  },
+  {
+    "revision": "434bbfe71f666964dab8dfdda07a1c17",
+    "url": "/collections/static/media/Grampa.434bbfe7.jpg"
+  },
+  {
+    "revision": "04a132ccf572a2f238cebfe26b3b2ef6",
+    "url": "/collections/static/media/Green lantern.04a132cc.jpg"
+  },
+  {
+    "revision": "7980ad569f2e110d59ebf8ad2ef1cc5d",
+    "url": "/collections/static/media/Homer.7980ad56.jpg"
+  },
+  {
+    "revision": "8fb26054e5cd2c507f29a48e474b9ee3",
+    "url": "/collections/static/media/Lisa.8fb26054.jpg"
+  },
+  {
+    "revision": "22ccef7a78b2a23dd3871a93e9a0e55d",
+    "url": "/collections/static/media/Lloyd.22ccef7a.jpg"
+  },
+  {
+    "revision": "86d8b573cd294e9a777318730afe5243",
+    "url": "/collections/static/media/Maggie.86d8b573.jpg"
+  },
+  {
+    "revision": "260e5e15d2395fd566dadb06b0e5ac3a",
+    "url": "/collections/static/media/Marge.260e5e15.jpg"
+  },
+  {
+    "revision": "8035dd93af322cbeb3d6fc077a4558f4",
+    "url": "/collections/static/media/Superman.8035dd93.jpg"
+  },
+  {
+    "revision": "e7ee545efb46228576f21cffdb64c6af",
+    "url": "/collections/static/media/Wu.e7ee545e.jpg"
+  }
+]);
